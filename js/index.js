8140f4e1bffbbd1b04d1aa8cf2abd4cbc2cf13cb
wloexPel <l.anastasia.platonova.l@gmail.com>
2023-03-25 15:28:18 -0600

    added current weather description

--- js/index.js
+++ js/index.js
@@ -20,6 +20,7 @@ function newTime() {
   let day = days[now.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
+
 function currentTemperature(response) {
   //let city = input.value;
   // city = input.value;
@@ -28,12 +29,16 @@ function currentTemperature(response) {
   let temperature = document.querySelector("#temp");
   let humidity = document.querySelector("#hum");
   let windSpeed = document.querySelector("#wind");
+  let currentDescription = document.querySelector("#current-description");
   let dateElement = document.querySelector("h2");
   let iconElement = document.querySelector(".curr-weather");
+
   city.innerHTML = response.data.city;
   celciusElement = response.data.temperature.current;
+
   temperature.innerHTML = Math.round(celciusElement);
   humidity.innerHTML = response.data.temperature.humidity;
+  currentDescription.innerHTML = response.data.condition.description;
   windSpeed.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = newTime(response.data.time * 1000);
   iconElement.setAttribute(
@@ -42,6 +47,7 @@ function currentTemperature(response) {
   );
   iconElement.setAttribute("alt", response.data.condition.description);
 }
+
 function searchCity(city) {
   let apiKey = "0544e76dbd90686t4c73ocb475aa7f05";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
@@ -55,6 +61,7 @@ function submitSearchForm(event) {
 
 let input = document.querySelector("#search-form");
 input.addEventListener("submit", submitSearchForm);
+
 function showFahrenheitTemp(event) {
   event.preventDefault();
   let fahrenheitTemperature = Math.round((celciusElement * 9) / 5 + 32);
@@ -63,6 +70,7 @@ function showFahrenheitTemp(event) {
   let temperature = document.querySelector("#temp");
   temperature.innerHTML = fahrenheitTemperature;
 }
+
 function showCelciusTemp(event) {
   event.preventDefault();
   let temperature = document.querySelector("#temp");
@@ -71,9 +79,11 @@ function showCelciusTemp(event) {
   temperature.innerHTML = Math.round(celciusElement);
 }
 let celciusElement = null;
+
 let fahrenheit = document.querySelector("#fahrenheit");
 fahrenheit.addEventListener("click", showFahrenheitTemp);
-//let celcius = document.querySelector("#celcius");
+
 let celcius = document.querySelector("#celcius");
 celcius.addEventListener("click", showCelciusTemp);
+
 searchCity("Kyiv");
