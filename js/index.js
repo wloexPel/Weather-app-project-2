7d4f7d026eb48d2be8f1304405af64bbcb7a6e31
wloexPel <l.anastasia.platonova.l@gmail.com>
2023-03-25 09:01:41 -0600

    added functions about current temperature and city

--- js/index.js
+++ js/index.js
@@ -1,6 +1,24 @@
+function currentTemperature(response) {
+  let city = input.value;
+  city.innerHTML = response.data.name;
+  let temperature = document.querySelector("#temp");
+  let humidity = document.querySelector("#hum");
+  let windSpeed = document.querySelector("#wind");
+  temperature.innerHTML = Math.round(response.data.main.temp);
+  humidity.innerHTML = response.data.main.humidity;
+  windSpeed.innerHTML = Math.round(response.data.wind.speed);
+}
+function defaultLocation(cityDefault) {
+  event.preventDefault();
+  let apiKey = "0544e76dbd90686t4c73ocb475aa7f05";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&units=metric&key=${apiKey}`;
+  axios.get(apiUrl).then(currentTemperature);
+}
 let apiKey = "0544e76dbd90686t4c73ocb475aa7f05";
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&units=metric&key=${apiKey}`;
 
-let searchInForm = document.querySelector("form");
-let input = document.querySelector("search-form");
-let city = Input.value;
+let input = document.querySelector("#search-form");
+searchForm.addEventListener("submit", findCity);
+let cityDefault = "Kiyv";
+defaultLocation(city);
+//let data = newData();
